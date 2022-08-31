876d06766b13c3ad757ec91ee180805ef5c1904b
holmista <tornikebuchukuri@redberry.ge>
2022-09-01 00:05:38 +0400

    refactor (quotes): extract movie-quote component

--- resources/views/client/quotes.blade.php
+++ resources/views/client/quotes.blade.php
@@ -1,22 +1,12 @@
 <x-bg>
     <x-switch-language locale='en' />
-    <div class="flex items-center flex-col h-screen w-screen">
+    <div class="flex items-center flex-col h-screen w-full">
         <h1 class="text-[48px] text-white mt-[79px] w-[750px]">father of soldior</h1>
         <div class="space-y-16 mt-20">
-            <div>
-                <div class="w-[750px] h-[350px] overflow-hidden flex justify-center items-center bg-red-400">
-                    <img src="https://georgianjournal.ge/media/_thumb/images/GJ/2013/20/levan-1.jpg" class="w-full">
-                </div>
-                <p class="text-[#3D3B3B] text-[36px] bg-white px-4 py-8 w-[750px] text-center">what should i tell your
-                    mom gnrjgnrjhrrnghhrtgherghrn?</p>
-            </div>
-            <div>
-                <div class="w-[750px] h-[350px] overflow-hidden flex justify-center items-center bg-red-400">
-                    <img src="https://georgianjournal.ge/media/_thumb/images/GJ/2013/20/levan-1.jpg" class="w-full">
-                </div>
-                <p class="text-[#3D3B3B] text-[36px] bg-white px-4 py-8 w-[750px] text-center">what should i tell your
-                    mom gnrjgnrjhrrnghhrtgherghrn?</p>
-            </div>
+            <x-movie-quote quote="what should i tell your mom gnrjgnrjhrrnghhrtgherghrn?"
+                image="https://georgianjournal.ge/media/_thumb/images/GJ/2013/20/levan-1.jpg" />
+            <x-movie-quote quote="what should i tell your mom gnrjgnrjhrrnghhrtgherghrn?"
+                image="https://georgianjournal.ge/media/_thumb/images/GJ/2013/20/levan-1.jpg" />
         </div>
     </div>
 </x-bg>
---
+++ resources/views/components/movie-quote.blade.php
@@ -0,0 +1,8 @@
+@props(['quote', 'image'])
+
+<div>
+    <div class="w-[750px] h-[350px] overflow-hidden flex justify-center items-center bg-red-400">
+        <img src="{{ $image }}" class="w-full">
+    </div>
+    <p class="text-[#3D3B3B] text-[36px] bg-white px-4 py-8 w-[750px] text-center">{{ $quote }}</p>
+</div>
