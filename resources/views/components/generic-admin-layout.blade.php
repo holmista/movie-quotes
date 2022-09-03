f77f8cdd0e68a73989dbe8133015efa8bb4a54fc
holmista <tornikebuchukuri@redberry.ge>
2022-09-03 23:07:48 +0400

    feat(admin-layout): quotes and movies links

--- resources/views/components/generic-admin-layout.blade.php
+++ resources/views/components/generic-admin-layout.blade.php
@@ -2,11 +2,14 @@
     <div class="sticky top-0 flex bg-red-400">
         <x-switch-language locale='en' type="admin" />
     </div>
-    {{-- <p class="text-lg {{ Request::is('admin/movies') ? 'text-blue-500' : 'text-gray-900' }}">
-        <a href="/admin/movies">movies</a>
-    </p>
-    <p class="text-lg {{ Request::is('admin/movies') ? 'text-blue-500' : 'text-gray-900' }}">
-        <a href="/admin">quotes</a>
-    </p> --}}
+    <div class="text-lg flex justify-end p-8 gap-x-4 sticky top-0">
+        <p class="{{ Request::is('admin/movies') ? 'text-blue-500' : 'text-gray-900' }}">
+            <a href="/admin/movies">movies</a>
+        </p>
+        <p class="{{ Request::is('admin') ? 'text-blue-500' : 'text-gray-900' }}">
+            <a href="/admin">quotes</a>
+        </p>
+    </div>
+
     {{ $slot }}
 </x-generic-layout>
