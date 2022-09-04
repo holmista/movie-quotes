8874637cdb1ad0ee689e1edc2d4964fa54b0b8ca
holmista <tornikebuchukuri@redberry.ge>
2022-09-04 22:49:01 +0400

    feat: switch languages in front

--- resources/views/components/generic-admin-layout.blade.php
+++ resources/views/components/generic-admin-layout.blade.php
@@ -1,6 +1,6 @@
 <x-generic-layout>
     <div class="sticky top-0 flex bg-red-400">
-        <x-switch-language locale='en' type="admin" />
+        <x-switch-language locale="{{ Config::get('app.locale') }}" type="admin" />
     </div>
     <div class="text-lg flex justify-end p-8 gap-x-4 sticky top-0">
         <p class="{{ Request::is('admin/movies') ? 'text-blue-500' : 'text-gray-900' }}">
--- resources/views/components/switch-language.blade.php
+++ resources/views/components/switch-language.blade.php
@@ -3,18 +3,23 @@
 @if ($type === 'client')
     <div class="h-screen flex items-center ml-14 absolute">
         <div class="space-y-4 w-fit">
-            <div
-                class="rounded-full w-14 h-14 border-solid border-white border-2
-                    font-normal text-2xl text-white flex items-center justify-center
-                    hover: cursor-pointer {{ $locale === 'en' ? 'bg-white text-[#171717]' : '' }}">
-                <span>en</span>
-            </div>
-            <div
-                class="rounded-full w-14 h-14 border-solid border-white border-2
+            <form method="get" action="/language/en">
+                @csrf
+                <button type="submit"
+                    class="rounded-full w-14 h-14 border-solid border-white border-2
+                        font-normal text-2xl text-white flex items-center justify-center
+                        hover: cursor-pointer {{ $locale === 'en' ? 'bg-white text-[#171717]' : '' }}">
+                    <span>en</span>
+                </button>
+            </form>
+            <form method="get" action="/language/ka">
+                <button type="submit"
+                    class="rounded-full w-14 h-14 border-solid border-white border-2
                     font-normal text-2xl text-white flex items-center justify-center
                     hover: cursor-pointer {{ $locale === 'ka' ? 'bg-white text-[#171717]' : '' }}">
-                <span>ka</span>
-            </div>
+                    <span>ka</span>
+                </button>
+            </form>
         </div>
     </div>
 @endif
@@ -22,18 +27,23 @@
 @if ($type === 'admin')
     <div class="h-screen flex items-center ml-14 absolute">
         <div class="space-y-4 w-fit">
-            <div
-                class="rounded-full w-14 h-14 border-solid border-blue-500 border-2
+            <form method="GET" action="/language/en">
+                @csrf
+                <button type="submit"
+                    class="rounded-full w-14 h-14 border-solid border-blue-500 border-2
                     font-normal text-2xl flex items-center justify-center text-[#171717]
                     hover: cursor-pointer {{ $locale === 'en' ? 'bg-blue-200' : '' }}">
-                <span>en</span>
-            </div>
-            <div
-                class="rounded-full w-14 h-14 border-solid border-blue-500 border-2
+                    <span>en</span>
+                </button>
+            </form>
+            <form method="GET" action="/language/ka">
+                <button type="submit"
+                    class="rounded-full w-14 h-14 border-solid border-blue-500 border-2
                     font-normal text-2xl flex items-center justify-center text-[#171717]
                     hover: cursor-pointer {{ $locale === 'ka' ? 'bg-blue-200' : '' }}">
-                <span>ka</span>
-            </div>
+                    <span>ka</span>
+                </button>
+            </form>
         </div>
     </div>
 @endif
