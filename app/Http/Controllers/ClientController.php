78170648d5ac41cb6dfff49661ff8664badabc6c
holmista <tornikebuchukuri@redberry.ge>
2022-09-05 18:07:45 +0400

    fix (client controller): return types

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,16 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Quote;
 use App\Models\Movie;
+use Illuminate\View\View;
 
 class ClientController extends Controller
 {
-	public function show()
+	public function show(): View
 	{
-		$randomQuote = Quote::with('movie')->inRandomOrder()->first(); //->movie()->get();
+		$randomQuote = Quote::with('movie')->inRandomOrder()->first();
 		return view('client.random-quote', ['quote'=>$randomQuote]);
 	}
 
-	public function index(Movie $movie)
+	public function index(Movie $movie): View
 	{
 		$movieQuotes = Movie::with('quotes')->where(['id'=>$movie->id])->orderBy('created_at', 'desc')->firstOrFail();
 		return view('client.quotes', ['movieQuotes'=>$movieQuotes]);
